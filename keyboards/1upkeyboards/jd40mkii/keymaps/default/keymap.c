473b3cccd7a8fbaf2231aeecbacde48b8f8befc1
Sonic45132 <sonicblue700@yahoo.com>
2019-11-29 19:54:21 -0500

    Update keymap to final version

--- keyboards/1upkeyboards/jd40mkii/keymaps/default/keymap.c
+++ keyboards/1upkeyboards/jd40mkii/keymaps/default/keymap.c
@@ -21,35 +21,53 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,-----------------------------------------------------------------------.
    * | Esc |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  | BSP |
    * |-----------------------------------------------------------------------+
-   * | Caps  |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  Enter  |
+   * |  Tab  |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  Enter  |
    * |-----------------------------------------------------------------------+
-   * |  Shift  |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  Alt  |Shift|
+   * |  Shift  |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |   .   |Shift|
    * |-----------------------------------------------------------------------+
-   * | Ctrl  | GUI | Alt |               Space               |  L1  |  Ctrl  |
+   * | Ctrl  | GUI | Alt |               Space               |  L1  |   L2   |
    * `-----------------------------------------------------------------------'
    */
   [0] = LAYOUT_40_ansi(
     KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
-    KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_ENT,
+    KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_ENT,
     KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_RSFT,
-    KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, MO(1), KC_RCTL
+    KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, MO(1), MO(2)
   ),
 
-  /* Function
+  /* Function 1
    * ,-----------------------------------------------------------------------.
-   * | F1  | F2  | F3  | F4  | F5  | F6  | F7  | F8  | F9  | F10 | F11 | Del |
+   * |     |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  | Del |
    * |-----------------------------------------------------------------------+
-   * |       |Left |Down |Right|     |     |     |     |     | Ins |  Home   |
+   * |       |     |     |     |     |     |     |     |     |     |         |
    * |-----------------------------------------------------------------------+
-   * |         |VolUp|VolDn|VolMu|     |     |     |     |     |  End  |PgDn |
+   * |         |VolUp|VolDn|VolMu|     |     |     |     |     |       |     |
    * |-----------------------------------------------------------------------+
    * |       |     |     |                                   |      |        |
    * `-----------------------------------------------------------------------'
    */
   [1] = LAYOUT_40_ansi(
-    KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_DEL,
-    KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_INS, KC_HOME,
-    KC_TRNS, KC_VOLU, KC_VOLD, KC_MUTE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_END, KC_PGDN,
+    KC_TRNS, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_DEL,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_VOLU, KC_VOLD, KC_MUTE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+  ),
+
+  /* Function 2
+   * ,-----------------------------------------------------------------------.
+   * |     |     | UP  |     |     |     |     |     |     |     |     |     |
+   * |-----------------------------------------------------------------------+
+   * |       |Left |Down |Right|     |     |     |     |     |     |         |
+   * |-----------------------------------------------------------------------+
+   * |         |     |     |     |     |     |     |     |     |       |     |
+   * |-----------------------------------------------------------------------+
+   * |       |     |     |                                   |      |        |
+   * `-----------------------------------------------------------------------'
+   */
+  [1] = LAYOUT_40_ansi(
+    KC_TRNS, KC_TRNS, KC_UP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   ),
 };
